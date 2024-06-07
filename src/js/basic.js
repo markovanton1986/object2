22a649266054df781a6e3a857931637b8ae741ad
markovanton1986 <markov_anton1986@mail.ru>
2024-06-07 15:35:16 +0600

    new

---
+++ src/js/basic.js
@@ -0,0 +1,16 @@
+export default function getAvailableAttackOptions(obj) {
+    const { special } = obj;
+    const result = [];
+    special.forEach((elem) => {
+      const {
+        id, name, icon, description = 'Описание недоступно',
+      } = elem;
+      result.push({
+        id,
+        name,
+        icon,
+        description,
+      });
+    });
+    return result;
+  }
